bfa1300a2caee9df8f107bc681fab3529910ccee
Chukka Yaswanth kumar <138193061+yaswanthchukka@users.noreply.github.com>
2024-03-01 10:00:09 +0530

    Add files via upload

---
+++ Bulb-(ON-OFF)/app.js
@@ -0,0 +1,27 @@
+function off(){
+    let background=document.getElementById('card');
+    // background.style.backgroundColor="white";
+    let img=document.getElementById('img');
+    img.src='off.jpg'
+    img.style.width="60%"
+    img.style.height="60%"
+    let btn1=document.getElementById('btn1')
+    let btn2=document.getElementById('btn2')
+    btn1.style.backgroundColor="Red"
+    btn2.style.backgroundColor="white"
+}
+
+
+function on(){
+    let background=document.getElementById('card');
+    // background.style.backgroundColor= "black";
+    let img=document.getElementById('img');
+    let btn1=document.getElementById('btn1')
+    let btn2=document.getElementById('btn2')
+    img.src='on.jpeg'
+    img.style.width="60%"
+    img.style.height="60%"
+    btn1.style.backgroundColor="white"
+    btn2.style.backgroundColor="green"
+
+}
